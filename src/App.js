776bb6a060fc47be8728a9441cad6cfab91eeccc
CaptainKhan <agaxanist@gmail.com>
2022-05-22 20:34:53 +0400

    ala bu backgroundu nece qoyum bu sekli bayaqdandi edemmirem aq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/bg.jpg"})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+},
+}));
+export default function App() {
+  const classes = useStyles();
+  return ( 
+  <div className={classes.root}>
+  {/* hele bir dayan gorek amk */}
+  </div>
+  )
 }
-
-export default App;
