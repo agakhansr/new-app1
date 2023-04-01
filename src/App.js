e80b629b458074b3453c7112f58b415886878992
azadsarxanli <azad.sarxanli@gmail.com>
2023-04-01 20:03:21 +0400

    my messsage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@mui/material/styles';
+import React from "react";
+import { makeStyles } from "@mui/material/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
+    height: "80vh",
     backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/bg.jpg"})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
-},
+  },
 }));
 export default function App() {
   const classes = useStyles();
-  return ( 
-  <div className={classes.root}>
-  <p>this is testing purpose</p>
-  </div>
-  )
+  return (
+    <div className={classes.root}>
+      <p>this is testing purpose</p>
+    </div>
+  );
 }
 //salamsalmsa
