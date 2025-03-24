f1559df675157dc64e0e327b70feaae3c06e5a1a
agakhansr <agaxanist@gmail.com>
2025-03-24 14:03:05 +0400

    testing isdone

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <p>this is testing purpose</p>
+      <p>testing is done</p>
     </div>
   );
 }
-//salamsalmsa[]\
-//asklasklsak
